a8030318b0d5406710b685a10f91af424f5bd2f6
Arul Jayanth <aruljayanth.m.unive@gmail.com>
2020-03-16 11:46:33 +0530

    v1.4.5

--- UIt/sel4.py
+++ UIt/sel4.py
@@ -8,25 +8,21 @@ signup_link = time_table + 'index1.php'
 
 #Home page
 driver.get(signup_link)
-print(driver.current_url)
 
 #Student module
 time.sleep(5)
 driver.find_element_by_id('ab').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #Faculty Course module
 driver.find_element_by_id('ab1').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #Faculty module
 driver.find_element_by_id('ab2').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #Admin module(clg admin)
@@ -43,42 +39,36 @@ submit = driver.find_element_by_id('login1').click()
 #assign courses
 driver.find_element_by_id('abc').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(5)
 
 #generate section
 driver.find_element_by_id('abc1').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
 #generate slots
 driver.find_element_by_id('abc2').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
 #edit timetable
 driver.find_element_by_id('abc3').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
 #delete timetable
 driver.find_element_by_id('abc4').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
 #generate timetable
 driver.find_element_by_id('abc5').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
@@ -93,25 +83,22 @@ time.sleep(5)
 driver.find_element_by_id('defaultForm-email').send_keys('mallikasachin@gmail.com')
 driver.find_element_by_id('defaultForm-pass').send_keys('admin')
 submit = driver.find_element_by_id('login').click()
-print(driver.current_url)
+
 #button in dept admin
 
 
 #add/remove faculty module
 driver.find_element_by_id('ac').click()
-print(driver.current_url)
 time.sleep(2)
 
 #add faculty module
 driver.find_element_by_id('abcd').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #remove faculty module
 driver.find_element_by_id('abcd1').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 driver.back()
@@ -120,73 +107,60 @@ time.sleep(5)
 #add/remove course module
 driver.find_element_by_id('ac1').click()
 time.sleep(2)
-print(driver.current_url)
-time.sleep(2)
 
 
 #add course module
 driver.find_element_by_id('abcd').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #remove course module
 driver.find_element_by_id('abcd1').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 driver.back()
 time.sleep(5)
 
-# #add/remove deparmtment module
-# driver.find_element_by_id('ac2').click()
-# time.sleep(2)
-# print(driver.current_url)
-# time.sleep(2)
-#
-#
-# #add deparmtment module
-# driver.find_element_by_id('abcd').click()
-# time.sleep(5)
-# print(driver.current_url)
-# driver.back()
-#
-# #remove deparmtment module
-# driver.find_element_by_id('abcd1').click()
-# time.sleep(5)
-# print(driver.current_url)
-# driver.back()
-# time.sleep(2)
-# driver.back()
-# time.sleep(5)
-#
-# #add/remove class/room
-# driver.find_element_by_id('ac3').click()
-# time.sleep(2)
-# print(driver.current_url)
-# driver.back()
-# time.sleep(2)
+#add/remove deparmtment module
+driver.find_element_by_id('ac2').click()
+time.sleep(2)
+
+
+#add deparmtment module
+driver.find_element_by_id('abcd').click()
+time.sleep(5)
+driver.back()
+
+#remove deparmtment module
+driver.find_element_by_id('abcd1').click()
+time.sleep(5)
+driver.back()
+time.sleep(2)
+driver.back()
+time.sleep(5)
+
+#add/remove class/room
+driver.find_element_by_id('ac3').click()
+time.sleep(2)
+driver.back()
+time.sleep(2)
 
 #add new semester
 driver.find_element_by_id('ac4').click()
 time.sleep(2)
-print(driver.current_url)
 driver.back()
 time.sleep(2)
 
 #logout
 driver.find_element_by_id('ac5').click()
-print(driver.current_url)
 
 #Feedback
 driver.find_element_by_id('ab4').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
 
 #class availability
 driver.find_element_by_id('ab5').click()
 time.sleep(5)
-print(driver.current_url)
 driver.back()
